6fee275616092781b4155be221a8d7655ef9b26a
Mateusz Urbanek <matnabru@gmail.com>
2021-11-28 22:37:39 +0100

    working on CORS

--- src/pages/posts/newpost.tsx
+++ src/pages/posts/newpost.tsx
@@ -1,24 +1,33 @@
 import { Layout } from '../../layouts/layout';
-import { getAllPostIds, getPostData } from '../../../lib/posts';
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    useQuery,
-    useMutation,
-    gql
-  } from "@apollo/client";
-import client from '@/lib/apollo-client';
-import { da } from 'date-fns/locale';
 import React from 'react';
 
 const NewPost = () => {
+    async function generateConversationWith(title: string, body: string) {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_HOST}`, {
+          method: "POST",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            query: `
+            mutation{
+                createArticle(article: { title: "${title}", body:"${body}"}) {
+                  title,
+                  body,
+                  createdAt
+                }
+              }`,
+          }),
+        });
+        const responseData = await response.json();
+    
+        console.log("response", responseData);
+      }
 
-    
-    
+
     const submitContact = async (event:any) => {
         event.preventDefault();
         alert(`So the title is ${event.target.title.value}?`);
+        generateConversationWith(event.target.title.value,event.target.body.value)
 
     };
 
